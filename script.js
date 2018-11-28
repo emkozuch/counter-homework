23782c4f8a8de1becc1023abd7258c6a81b27c55
emkozuch <emkozuch@gmail.com>
2018-11-28 09:22:03 +0100

    fiex code

--- script.js
+++ script.js
@@ -4,30 +4,17 @@
 
     function changeNumber() {
         var addButton = document.getElementById("add");
-        var subbbutton = document.getElementById("substract")
-        var initialValue = 0;
+        var subButton = document.getElementById("substract")
+        var value = 0;
 
-        addButton.onclick = function () {
-            initialValue += 1;
-            document.getElementById("counter").innerHTML = initialValue;
-        }
-        addButton.onmousedown = function () {
-            addButton.style.backgroundColor = "rgba(35, 80, 24, 0.5)";
-        }
-        addButton.onmouseup = function () {
-            addButton.style.backgroundColor = "rgba(128, 15, 15, 0.1)";
-        }
-
-        subbbutton.onclick = function () {
-            initialValue -= 1;
-            document.getElementById("counter").innerHTML = initialValue;
-        }
-        subbbutton.onmousedown = function () {
-            subbbutton.style.backgroundColor = "rgba(71, 22, 16, 0.5)";
-        }
-        subbbutton.onmouseup = function () {
-            subbbutton.style.backgroundColor = "rgba(128, 15, 15, 0.1)";
-        }
+        addButton.addEventListener("click", function () {
+            value += 1;
+            document.getElementById("counter").innerHTML = value;
+        })
+        subButton.addEventListener("click", function () {
+            value -= 1;
+            document.getElementById("counter").innerHTML = value;
+        })
     }
 
 
